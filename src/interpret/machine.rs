9a248478b8b6046cd9aae95255af6a36872e39a8
bpaul <bpaul@bpaul.xyz>
2024-05-12 02:22:48 +1000

    Implement machine instruction interpetation with enums

--- src/interpret/machine.rs
+++ src/interpret/machine.rs
@@ -3,11 +3,32 @@ use crate::{instructions::machine::MachineInstruction, Emulator, Privilege};
 impl Emulator {
     pub fn execute_machine(&mut self, instruction: MachineInstruction) {
         if self.privilege < Privilege::Machine {
-            todo!();
+            // TODO i wasn't able to figure out what should happen in this case
+            // from reading the specification, so I'm just going to assume
+            // invalid instruction
+            self.illegal_instruction();
         }
         match instruction {
-            MachineInstruction::MRet(_) => todo!(),
-            MachineInstruction::Wfi(_) => todo!(),
+            MachineInstruction::MRet(_) => {
+                self.pc = self.mepc.wrapping_sub(4);
+                // Set MIE to MPIE
+                self.mstatus = (self.mstatus & !0x8) | (self.mstatus & !0x80) >> 4;
+                // Set privilege to the value in MPP
+                self.privilege = (self.mstatus >> 11 & 0x3)
+                    .try_into()
+                    .expect("An illegal MPP value was written to mstatus.");
+                // Set MPIE to 1
+                self.mstatus |= 0x80;
+                // Set MPP to user mode
+                self.mstatus &= !(3 << 11);
+                // If we are not in machine mode, set MPRV to 0
+                if self.privilege != Privilege::Machine {
+                    self.mstatus &= !(1 << 17);
+                }
+            }
+            MachineInstruction::Wfi(_) => {
+                self.waiting = true;
+            },
         }
     }
 }
