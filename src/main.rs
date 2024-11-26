9f7c3a279d0551ed7856a480f86d4f9357f35311
bpaul <bpaul@bpaul.xyz>
2024-11-26 18:39:04 +1000

    Untested code for running riscof tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
-use riscv::{Emulator, device::Device, tester::Tester};
+use clap::Parser;
+
+use riscv::{device::Device, tester::Tester, Emulator};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Path to the executable to run
+    executable: String,
+    /// Path to a signature file to output (when running riscof tests)
+    #[arg(long)]
+    signature: String,
+}
 
 fn main() {
-    let path = std::env::args().nth(1).unwrap();
+    let args = Args::parse();
+
+    let path = args.executable;
 
     let mut emu = Emulator::new(128 * 1024 * 1024);
 
-    emu.load_binary(&path, 0x1000).unwrap();
+    let elf = emu.load_binary(&path).unwrap();
 
-    let tester_addr = 0x3000;
+    let tester_addr = elf.get_symbol("tohost").unwrap().value;
+    let signature_start = elf.get_symbol("begin_signature").unwrap().value;
+    let signature_end = elf.get_symbol("end_signature").unwrap().value;
+    println!("tester_addr: {:x}", tester_addr);
+    println!("signature_start: {:x}", signature_start);
+    println!("signature_end: {:x}", signature_end);
 
     let tester = Rc::new(RefCell::new(Tester::new(tester_addr)));
 
     emu.add_device(tester.clone() as Rc<RefCell<dyn Device>>);
+
+    loop {
+        emu.cycle();
+        if let Some(code) = tester.borrow().get_exit_code() {
+            println!("{code}");
+            emu.write_signature(&args.signature, signature_start, signature_end)
+                .unwrap();
+            break;
+        }
+    }
 
     /*
     for entry in std::fs::read_dir(path).unwrap().flatten() {
